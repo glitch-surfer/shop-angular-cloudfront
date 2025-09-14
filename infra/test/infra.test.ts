1025e568405adf7ece19fd81be9e4868ad76ed01
vladislav_garaev <vladislav_garaev@epam.com>
2025-09-14 17:30:52 +0200

    feat: rename infra-stack.ts and add deployment

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -3,11 +3,11 @@
 // import * as Infra from '../lib/infra-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
+// example resource in lib/deploy-web-app-stack.ts
 test('SQS Queue Created', () => {
   //   const app = new cdk.App();
   //     // WHEN
-  //   const stack = new Infra.InfraStack(app, 'MyTestStack');
+  //   const stack = new Infra.DeployWebAppStack(app, 'MyTestStack');
   //     // THEN
   //   const template = Template.fromStack(stack);
   //   template.hasResourceProperties('AWS::SQS::Queue', {
